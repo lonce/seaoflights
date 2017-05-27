80c61e3e7b4d5d7ca2a80c37de77a4aed510113d
mclonce <lonce.wyse@nus.edu.sg>
2017-05-27 15:23:18 -0700

    comments

--- seaoflightsServer.js
+++ seaoflightsServer.js
@@ -16,6 +16,7 @@ if (process.argv.length < 3){
 k_portnum=process.argv[2] || k_portnum;
 
 //****************************************************************************
+// sol is the location that nginx looks for to reverse proxy
 var m_useRoot="/sol/www/";
 app.use(express.static(__dirname + m_useRoot));
 
