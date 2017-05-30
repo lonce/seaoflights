577fe66369d9f28508d8b477492ebe9956103905
Kerem Goksel <bkgoksel@gmail.com>
2017-05-29 22:55:18 -0700

    Wrote the client side websocket and infrastructure

---
+++ www/movements.js
@@ -0,0 +1,51 @@
+var firstMovement = {
+  init: function() {
+    this.meter = new p5.Amplitude();
+    this.osc = new p5.SinOsc();
+    this.env = new p5.Env();
+    this.env.setADSR(0.01, 0.1, 1, 0.5);
+    this.env.setRange(1, 0);
+    this.osc.amp(this.env);
+    this.osc.start();
+  },
+  draw: function() {
+    var level = this.meter.getLevel();
+    var bgColor = color(
+        config.visual.bg.H,
+        config.visual.bg.S,
+        config.visual.bg.B,
+        pow(level, config.visual.bg.alphaFactor));
+
+    noStroke();
+    fill(bgColor);
+    rect(0, 0, width, height);
+  },
+  messageHandler: {
+    "init": globalMessageHandler.init,
+    "seatingAck": globalMessageHandler.seatingAck,
+    "mute": globalMessageHandler.mute,
+    "setMovement": globalMessageHandler.setMovement,
+    "setNote": this.setNote
+  },
+  touchStarted: function() {
+    this.noteOn();
+  },
+  touchEnded: function() {
+    this.noteOff();
+  },
+  noteOn: function() {
+    this.env.triggerAttack();
+  },
+  noteOff: function() {
+    this.env.triggerRelease();
+  },
+  setNote: function(payload) {
+    this.osc.freq(midiToFreq(payload.note));
+  }
+}
+
+var movements = [
+  firstMovement
+]
+
+
