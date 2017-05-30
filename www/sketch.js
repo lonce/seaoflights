6ef58c242d3b24a7e0461cf42bdf5ca1be674f0b
Kerem Goksel <bkgoksel@gmail.com>
2017-05-29 22:55:08 -0700

    Wrote the client side websocket and infrastructure

--- www/sketch.js
+++ www/sketch.js
@@ -1,139 +1,102 @@
-var devices = {
-  osc: {
-    sin: null,
-    saw: null,
-  },
-  env: {
-      sin: null,
-      saw: null
-  },
-  meter: null,
-  fft: null
-}
-
 var state = {
-  rotation: 0
+  clientId: -1,
+  seatingSection: "C",
+  movement: -1
+};
+
+var socket = null;
+
+var globalMessageHandler = {
+  "init": initClient,
+  "seatingAck": seatingCheck,
+  "mute": muteClient,
+  "setMovement": setMovement
 }
 
-
-function setup() {
-    frameRate(config.frameRate);
-    setupAudio();
-    setupVisuals();
-
-    //fullscreen(true);
-
-    colorMode(HSB, 100, 100, 100, 1);
-    createCanvas(windowWidth, windowHeight);
+function audienceInit() {
+  // TODO: Make these pretty
+  alert("Please make sure your phone isn't silenced, and the volume is turned up.");
+  alert("Please make sure your device rotation is locked, and your device doesn't go to sleep.");
+  alert("If you ever have issues with the instrument, please reload the page and your instrument will be reinitialized. Enjoy!");
+  state.seatingSection = prompt("Enter the general area of the audience you're seated at(this doesn't have to be exact) : (L)eft, (C)enter, (R)ight", "");
+  conn = io.connect(conn.serverAddr);
+  conn.on('message', handleMessage);
 }
 
-function deviceMoved() {
-  if (config.visual.moveWave) {
-    var rot = rotationX;
-    if (rotationX > 40) {
-      rot = 40;
-    } else if (rotationX < -10) {
-      rot = -10;
-    }
-    state.rotation = (rot + 10)/50.0;
-    devices.env.sin.setRange(state.rotation, 0);
-    devices.env.saw.setRange(1-state.rotation, 0);
+function initClient(payload) {
+  state.clientId = payload.clientId;
+  console.log("Server initialized this client with the id ", state.clientId, ", responding with the seating section info, ", state.seatingSection);
+  conn.send({clientId: state.clientId, seatingSection: state.seatingSection});
+}
+
+function seatingCheck(payload) {
+  if(payload.seatingSection !== state.seatingSection) {
+    console.log("Server has outdated seating information for me, correcting");
+    sendMessage({clientId: state.clientId, seatingSection: state.seatingSection});
+  } else {
+    console.log("Server has correct seating information for me, moving on");
   }
 }
 
-function drawBG() {
-    var level = devices.meter.getLevel();
-    var bgColor = color(
-        config.visual.bg.H,
-        config.visual.bg.S,
-        config.visual.bg.B,
-        pow(level, config.visual.bg.alphaFactor));
 
-    noStroke();
-    fill(bgColor);
-    rect(0, 0, width, height);
+function handleMessage(msg) {
+  if(msg.destId === "all" || msg.destId === clientId) {
+    if(state.movement >= 0) {
+      state.movement.messageHandler[msg.header](msg.payload);
+    } else {
+      globalMessageHandler[msg.header](msg.payload);
+    }
+  }
 }
 
-function drawWave() {
-    var waveform = devices.fft.waveform();  // analyze the waveform
-    beginShape();
-    strokeWeight(config.visual.stroke.width);
-    stroke(0, 0, 0, config.visual.stroke.alpha);
-    fill(0, 0, 0, 0);
-    var waveTop = height;
-    var waveBot = 0;
-    if (config.visual.moveWave) {
-      var bottom_offset = state.rotation * (height - config.visual.waveHeight)
-      waveTop = bottom_offset + config.visual.waveHeight;
-      waveBot = bottom_offset;
-    }
-    for (var i = 0; i < waveform.length; i++){
-        var x = map(i, 0, waveform.length, 0, width);
-        var y = map(waveform[i], -1, 1, waveTop, waveBot);
-        vertex(x, y);
-    }
-    endShape();
+function mute(payload) {
+  console.log("Muting client");
+}
+
+function setMovement(payload) {
+  state.movement = -1;
+  console.log("Unsetting movement so new movement can be initialized");
+  movements[payload.movement].init();
+  console.log("Setting movement to: ", payload.movement);
+  state.movement = payload.movement;
+}
+
+function setup() {
+  frameRate(config.frameRate);
+  audienceInit();
+  connectToServer();
+  colorMode(HSB, 100, 100, 100, 1);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-    background(0);
-    drawBG();
-    drawWave();
-}
-
-function setupVisuals() {
-    devices.fft = new p5.FFT();
-}
-
-function getEnv() {
-    var env = new p5.Env();
-    env.setADSR(
-        config.audio.env.attackTime,
-        config.audio.env.decayTime,
-        config.audio.env.susPercent,
-        config.audio.env.releaseTime);
-
-    env.setRange(1, 0);
-    return env;
-}
-
-function setupAudio() {
-    devices.meter = new p5.Amplitude(0.10);
-
-    devices.osc.sin = new p5.SinOsc();
-    devices.env.sin = getEnv();
-    devices.osc.sin.amp(devices.env.sin);
-    devices.osc.sin.start();
-
-    devices.osc.saw = new p5.SawOsc();
-    devices.env.saw = getEnv();
-    devices.osc.saw.amp(devices.env.saw);
-    devices.osc.saw.start();
-}
-
-function tuneSynths(note) {
-    config.visual.bg.H = note % 100;
-    devices.osc.sin.freq(midiToFreq(note));
-    devices.osc.saw.freq(midiToFreq(note));
-}
-
-function noteOn(note) {
-    tuneSynths(note + config.audio.noteOffset);
-    devices.env.sin.triggerAttack();
-    devices.env.saw.triggerAttack();
-}
-
-function noteOff() {
-    devices.env.sin.triggerRelease();
-    devices.env.saw.triggerRelease();
+  if (state.movement > -1 && state.movement.draw) {
+    state.movement.draw();
+  } else {
+    console.log("No phase set, skipping draw");
+  }
 }
 
 function touchStarted() {
-    noteOn(48);
+  if(state.movement > -1 && state.movement.touchStarted) {
+    state.movement.touchStarted();
+  }
 }
 
 function touchEnded() {
-    noteOff();
+  if(state.movement > -1 && state.movement.touchEnded) {
+    state.movement.touchEnded();
+  }
 }
 
+function deviceShaken() {
+  if(state.movement > -1 && state.movement.deviceShaken) {
+    state.movement.deviceShaken();
+  }
+}
 
+function deviceMoved() {
+  if(state.movement > -1 && state.movement.deviceMoved) {
+    state.movement.deviceMoved();
+  }
+}
